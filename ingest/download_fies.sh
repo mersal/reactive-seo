4fe7e23306eba450ae3e06edf7db8cf954e6ec40
Mersal <mersal@gmai.com>
2015-06-16 20:28:20 -0700

    download/extract wikipedia database dump

---
+++ ingest/download_fies.sh
@@ -0,0 +1,1 @@
+wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
---
+++ ingest/extract_files.sh
@@ -0,0 +1,2 @@
+bunzip2 enwiki-latest-pages-articles.xml.bz2
+mv enwiki-latest-pages-articles.xml ~/reactive-seo/data/wikipedia/input/
