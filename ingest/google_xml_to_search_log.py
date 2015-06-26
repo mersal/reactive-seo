21260ea3cefcac5057b1028d9264f65f9db9107d
Mersal <mersal@gmai.com>
2015-06-25 23:05:05 -0700

    parse description tag

--- ingest/google_xml_to_search_log.py
+++ ingest/google_xml_to_search_log.py
@@ -5,6 +5,8 @@ import wget, time, os, random
 
 ts = int(time.time())
 ts2 = ts - 15552000
+ts3 = ts + 2635932
+ts4 = ts - 86400
 
 path_data_ht = '/home/ubuntu/reactive-seo/data/hot-trends/'
 path_data_sl = '/home/ubuntu/reactive-seo/data/search-logs/'
@@ -17,8 +19,22 @@ fo = open(filename_out, "w")
 f = wget.download(url, filename_xml)
 root = ET.parse(f).getroot()
 for a in root.findall("channel/item/title"):
-  for num in range(1, 10000):
-    x = str(random.randrange(ts2, ts))
+  print "processing trend: " + a.text
+  for num in range(1, random.randrange(10000,50000)):
+    x = str(random.randrange(ts4, ts))
     fo.write(x + "\t" + a.text + "\t")
     fo.write("\n")
+for a in root.findall("channel/item/description"):
+  if a.text:
+    print "processing trend: " + a.text
+    words = a.text.split(', ')
+    for word in words:
+      for num in range(1, random.randrange(10000,50000)):
+        x = str(random.randrange(ts4, ts))
+        fo.write(x + "\t" + a.text + "\t")
+        fo.write("\n")
 fo.close()
+
+#if os.path.exists(filename_out):
+#  os.system("hdfs dfs -copyFromLocal -f " + filename_out + " /reactive-seo/data/search-logs/")
+
