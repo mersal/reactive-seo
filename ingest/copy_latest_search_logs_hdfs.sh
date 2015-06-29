a834ed41b26000c94212b67dcd73be67a3c9b3f7
Mersal <mersal@gmai.com>
2015-06-28 19:29:19 -0700

    update

--- ingest/copy_latest_search_logs_hdfs.sh
+++ ingest/copy_latest_search_logs_hdfs.sh
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-for FILE in `ls ~/reactive-seo/data/search-logs | sort -gr | head -1`
-	do hdfs dfs -copyFromLocal -f ~/reactive-seo/data/search-logs/$FILE /reactive-seo/data/search-logs/
-done
-=======
 # script to copy the latest search logs file onto HDFS
 for FILE in `ls ~/reactive-seo/data/search-logs | sort -gr | head -1`
 	do hdfs dfs -copyFromLocal -f ~/reactive-seo/data/search-logs/$FILE /reactive-seo/data/search-logs/
 done
->>>>>>> 29d4439e956a7ebfb7b0856d87af6ba218a4c525
