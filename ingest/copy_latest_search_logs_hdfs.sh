6585647c28deda9d26e6e605861b5ac7d349e7b9
Mersal <mersal@gmai.com>
2015-07-17 22:13:19 -0700

    update all files final

--- ingest/copy_latest_search_logs_hdfs.sh
+++ ingest/copy_latest_search_logs_hdfs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # script to copy the latest search logs file onto HDFS
 for FILE in `ls ~/reactive-seo/data/search-logs | sort -gr | head -1`
 	do hdfs dfs -copyFromLocal -f ~/reactive-seo/data/search-logs/$FILE /reactive-seo/data/search-logs/
