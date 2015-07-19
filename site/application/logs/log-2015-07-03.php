5e5ecf52996fa886005d395fc152bd2c4c741533
mersal <mersal@gmail.com>
2015-07-19 12:36:44 -0700

    remove old log files

--- site/application/logs/log-2015-07-03.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2015-07-03 01:40:53 --> Severity: Notice  --> Undefined index: seo_result /Users/mersal/Projects/reactive-seo/site/application/views/results.php 6
-ERROR - 2015-07-03 01:40:53 --> Severity: Warning  --> Invalid argument supplied for foreach() /Users/mersal/Projects/reactive-seo/site/application/views/results.php 7
-ERROR - 2015-07-03 01:41:26 --> Severity: Notice  --> Undefined index: seo_result /Users/mersal/Projects/reactive-seo/site/application/views/results.php 8
-ERROR - 2015-07-03 01:41:26 --> Severity: Warning  --> Invalid argument supplied for foreach() /Users/mersal/Projects/reactive-seo/site/application/views/results.php 9
